d340d87b0b8378ac8d22701521544aa67e0ac536
hrz <huangruizhe@live.cn>
2019-01-29 20:15:36 +0800

    final update

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dbUsername = 'tornado'
-dbPassword = 'tornado'
-dbHost = '139.196.96.35'
-dbPort = 3306
-dbDatabase = 'dbDesign'
-
-current_path = os.path.dirname(__file__)
--- static/config.js
+++
@@ -1,3 +0,0 @@
-let serverUrl = "http://139.196.96.35";
-let port = "80";
-let baseUrl = serverUrl + ":" + port;
